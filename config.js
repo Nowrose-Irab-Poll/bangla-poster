a57bdf0bece850f5a471051f85b1fed856478a11
Mahmud26 <mahmudur@revechat.com>
2024-03-17 21:45:38 +0600

    home screen

---
+++ config.js
@@ -0,0 +1,3 @@
+var CONFIG = {
+    "ROOT_DOMAIN":"http://127.0.0.1/"
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+
+  
+// document.getElementById("grid-banner").addEventListener('click', function(){
+
+//     console.log("clicked")
+// })
+
+document.addEventListener('DOMContentLoaded', function() {
+    var rowItems = document.querySelectorAll('#grid-container-row #grid-banner .col');
+  
+    rowItems.forEach(function(item) {
+      item.addEventListener('click', function(event) {
+        var param1 = 'image';
+        var imageUrl = event.target.src.split("/").pop();
+        var queryString = encodeURIComponent(param1) + '=' + encodeURIComponent(imageUrl);
+        var url = CONFIG.ROOT_DOMAIN+ "poster-engine.html?" + queryString;
+  
+  
+        window.location.href = url;
+      });
+    });
+  });
