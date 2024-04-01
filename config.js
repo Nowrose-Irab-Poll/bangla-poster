2f3411fe7d61fc2d448d20504032c8ade3e7eb03
Mahmud26 <mahmudur@revechat.com>
2024-04-01 22:13:33 +0600

    registration complete

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 var CONFIG = {
-    "ROOT_DOMAIN":"http://127.0.0.1:5500/"
+    "ROOT_DOMAIN":"http://127.0.0.1:5500/",
+    "HOST_URL": "http://157.230.35.195:81/"
 }
--- registration.js
+++ registration.js
@@ -3,6 +3,10 @@ document.addEventListener('DOMContentLoaded', function(){
         if($_BANGLA_BANNER.validateSignUp()) {
             document.getElementById("register-button").style.display='none';
             document.getElementById("register-spinner").style.display = 'flex';
+            var name = document.getElementById("register-name").value;
+            var phone = document.getElementById("register-phone").value;
+            var password = document.getElementById("register-password").value;
+            $_BANGLA_BANNER.registraterUser(phone,name,password);
         }
     })
 });
@@ -31,5 +35,48 @@ $_BANGLA_BANNER = {
             return false;
         }
         return true;
+    },
+    registraterUser: function(phone,name,password) {
+        var registerUrl = CONFIG.HOST_URL + "api/user/register";
+        const registerData = {
+            "phone":phone,
+            "name":name,
+            "password":password
+        };
+        fetch(registerUrl, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(registerData)
+          })
+          .then(response => {
+            switch(response.status) {
+                case 409:
+                    document.getElementById("register-button").style.display='flex';
+                    document.getElementById("register-spinner").style.display = 'none';
+                    document.getElementById("registration-status").innerText = "User with same email or phone already exists";
+                    throw new Error("User with same email or phone already exists");
+                case 201:
+                    var responseData = response.json();
+                    document.getElementById("register-button").style.display='flex';
+                    document.getElementById("register-spinner").style.display = 'none';
+                    document.getElementById("registration-status").innerText = "";
+                    window.location.href = CONFIG.ROOT_DOMAIN + "signin.html";
+                    break;
+                default:
+                    console.error(response.json());
+                    document.getElementById("register-button").style.display='flex';
+                    document.getElementById("register-spinner").style.display = 'none';
+                    document.getElementById("registration-status").innerText = "Oh snap!! Something went wrong. Might be Aliens invaded us or we crashed";
+                    throw new Error("Oh snap! Something went wrong");
+
+            }
+          })
+          .catch(error => {
+            // Handle any errors that occurred during the fetch
+            console.error('Error:', error);
+            return "Oh snap! Something went wrong";
+          });
     }
 }
